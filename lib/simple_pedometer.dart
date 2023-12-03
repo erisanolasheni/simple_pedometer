8d8f66d00278436cc9c55d3df4d0d7120ebd5af3
Erisan Olasheni <erisanolasheni@gmail.com>
2023-12-03 04:44:30 +0100

    Update simple_pedometer.dart

--- lib/simple_pedometer.dart
+++ lib/simple_pedometer.dart
@@ -1,15 +1,11 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:flutter/services.dart';
-
 class SimplePedometer {
   static const MethodChannel _channel = MethodChannel('simple_pedometer');
 
   static Future<int> getTodaysSteps() async {
     final now = DateTime.now();
     final todayStartedAt = now.add(
-        Duration(hours: -now.hour, minutes: -now.minute, seconds: -now.second));
+      Duration(hours: -now.hour, minutes: -now.minute, seconds: -now.second),
+    );
     return getSteps(from: todayStartedAt, to: now);
   }
 
@@ -23,13 +19,26 @@ class SimplePedometer {
     }
     final args = <String, dynamic>{
       'startTime': from.millisecondsSinceEpoch,
-      'endTime': to.millisecondsSinceEpoch
+      'endTime': to.millisecondsSinceEpoch,
     };
-    final res = await _channel.invokeMethod<int>(
-      'getSteps',
-      args,
-    ) ??
-        0;
+    final res = await _channel.invokeMethod<int>('getSteps', args) ?? 0;
+    return res;
+  }
+
+  static Future<double> getWalkingDuration({
+    required DateTime from,
+    required DateTime to,
+  }) async {
+    if (!Platform.isIOS) {
+      assert(false, 'This plugin only supports iOS');
+      return 0.0;
+    }
+    final args = <String, dynamic>{
+      'startTime': from.millisecondsSinceEpoch,
+      'endTime': to.millisecondsSinceEpoch,
+    };
+    final res =
+        await _channel.invokeMethod<double>('getWalkingDuration', args) ?? 0.0;
     return res;
   }
 }
