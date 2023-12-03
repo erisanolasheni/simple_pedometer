d7abdad8fb8e63ce8a2ec33b75ef05f17f455384
Erisan Olasheni <erisanolasheni@gmail.com>
2023-12-03 04:34:48 +0100

    Update SwiftSimplePedometerPlugin.swift

--- ios/Classes/SwiftSimplePedometerPlugin.swift
+++ ios/Classes/SwiftSimplePedometerPlugin.swift
@@ -4,8 +4,8 @@ import CoreMotion
 
 public class SwiftSimplePedometerPlugin: NSObject, FlutterPlugin {
     
-    let pedometer = CMPedometer();
-    var channelResult: FlutterResult?;
+    let pedometer = CMPedometer()
+    var channelResult: FlutterResult?
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "simple_pedometer", binaryMessenger: registrar.messenger())
@@ -14,28 +14,54 @@ public class SwiftSimplePedometerPlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        self.channelResult = result;
-        if (call.method.elementsEqual("getSteps")){
+        self.channelResult = result
+        if call.method.elementsEqual("getSteps") {
             getSteps(call: call)
-        }  }
+        } else if call.method.elementsEqual("getWalkingDuration") {
+            getWalkingDuration(call: call)
+        }
+    }
     
     func getSteps(call: FlutterMethodCall) {
         guard let arguments = call.arguments as? NSDictionary,
-              let startTime = (arguments["startTime"] as? NSNumber),
-              let endTime = (arguments["endTime"] as? NSNumber)
+              let startTime = arguments["startTime"] as? NSNumber,
+              let endTime = arguments["endTime"] as? NSNumber
         else {
-            self.channelResult!(0);
-            return;
+            self.channelResult?(0)
+            return
         }
+        
         let dateFrom = Date(timeIntervalSince1970: startTime.doubleValue / 1000)
         let dateTo = Date(timeIntervalSince1970: endTime.doubleValue / 1000)
-        pedometer.queryPedometerData(from: dateFrom, to: dateTo, withHandler: {(data, error) in
-            if (error == nil){
-                let steps = data!.numberOfSteps;
-                self.channelResult!(steps.intValue)
-                return;
+        
+        pedometer.queryPedometerData(from: dateFrom, to: dateTo, withHandler: { (data, error) in
+            if let data = data, error == nil {
+                let steps = data.numberOfSteps
+                self.channelResult?(steps.intValue)
             } else {
-                self.channelResult!(0)
+                self.channelResult?(0)
+            }
+        })
+    }
+
+    func getWalkingDuration(call: FlutterMethodCall) {
+        guard let arguments = call.arguments as? NSDictionary,
+              let startTime = arguments["startTime"] as? NSNumber,
+              let endTime = arguments["endTime"] as? NSNumber
+        else {
+            self.channelResult?(0)
+            return
+        }
+        
+        let dateFrom = Date(timeIntervalSince1970: startTime.doubleValue / 1000)
+        let dateTo = Date(timeIntervalSince1970: endTime.doubleValue / 1000)
+        
+        pedometer.queryPedometerData(from: dateFrom, to: dateTo, withHandler: { (data, error) in
+            if let data = data, error == nil {
+                let walkingDuration = data.walkingDuration?.doubleValue ?? 0
+                self.channelResult?(walkingDuration)
+            } else {
+                self.channelResult?(0)
             }
         })
     }
