fde0b68eb2fffb8c8c08caae276e13d471cb49f6
Erisan Olasheni <erisanolasheni@gmail.com>
2023-12-10 05:23:07 +0100

    Update SwiftSimplePedometerPlugin.swift

--- ios/Classes/SwiftSimplePedometerPlugin.swift
+++ ios/Classes/SwiftSimplePedometerPlugin.swift
@@ -44,26 +44,67 @@ public class SwiftSimplePedometerPlugin: NSObject, FlutterPlugin {
         })
     }
 
-    func getWalkingDuration(call: FlutterMethodCall) {
-    guard let arguments = call.arguments as? NSDictionary,
+    func calculateTotalActiveTime(startDate: Date, endDate: Date) {
+
+    var previousStepCount: Int = 0
+    var totalActiveTimeInSeconds: TimeInterval = 0
+
+    // Define the time interval for querying step count data
+    let queryInterval: TimeInterval = 5 // 5 seconds interval
+
+    let dispatchGroup = DispatchGroup()
+
+    // Iterate over smaller intervals and query step count data
+    for intervalStart in stride(from: startDate.timeIntervalSinceReferenceDate, to: endDate.timeIntervalSinceReferenceDate, by: queryInterval) {
+        let intervalEnd = min(intervalStart + queryInterval, endDate.timeIntervalSinceReferenceDate)
+        let intervalStartDate = Date(timeIntervalSinceReferenceDate: intervalStart)
+        let intervalEndDate = Date(timeIntervalSinceReferenceDate: intervalEnd)
+
+        dispatchGroup.enter()
+
+        pedometer.queryPedometerData(from: intervalStartDate, to: intervalEndDate) { pedometerData, error in
+            defer {
+                dispatchGroup.leave()
+            }
+
+            if let pedometerData = pedometerData {
+                let currentStepCount = pedometerData.numberOfSteps.intValue
+                let stepCountDifference = currentStepCount - previousStepCount
+
+                if stepCountDifference > 0 {
+                    // If steps were taken in this interval, add the interval duration to totalActiveTimeInSeconds
+                    totalActiveTimeInSeconds += intervalEndDate.timeIntervalSince(intervalStartDate)
+                }
+
+                previousStepCount = currentStepCount
+            } else if let error = error {
+                print("Error: \(error.localizedDescription)")
+                self.channelResult?(0) // Use self.channelResult directly
+                return
+            }
+        }
+    }
+
+    dispatchGroup.notify(queue: .main) {
+        print("Swift:::: I am done here.");
+        // At the end of the iteration, totalActiveTimeInSeconds will contain the total active time duration
+        self.channelResult?(totalActiveTimeInSeconds) // Use self.channelResult directly
+    }
+}
+
+func getWalkingDuration(call: FlutterMethodCall) {
+    guard let arguments = call.arguments as? [String: Any],
           let startTime = arguments["startTime"] as? NSNumber,
           let endTime = arguments["endTime"] as? NSNumber
     else {
         self.channelResult?(0)
         return
     }
-    
+
     let dateFrom = Date(timeIntervalSince1970: startTime.doubleValue / 1000)
     let dateTo = Date(timeIntervalSince1970: endTime.doubleValue / 1000)
-    
-    pedometer.queryPedometerData(from: dateFrom, to: dateTo) { (data, error) in
-        if let data = data, error == nil {
-            let walkingDuration = data.activeTime?.doubleValue ?? 0
-            self.channelResult?(walkingDuration)
-        } else {
-            self.channelResult?(0)
-        }
-    }
+
+    calculateTotalActiveTime(startDate: dateFrom, endDate: dateTo)
 }
 
 }
